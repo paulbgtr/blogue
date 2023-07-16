d42940ed9f4be5b31a77a86836008015773b3bc8
Gibsol <gibsol75@gmail.com>
2023-07-16 03:56:35 +0300

    :sparkles: Index page implement Hero component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,25 @@
+import Link from "next/link";
+
 import { Hero } from "@/components/Hero";
 
 export default function Home() {
-  return <Hero />;
+  return (
+    <Hero>
+      <h1 className="text-5xl font-bold">blogue</h1>
+      <p className="py-6">
+        Explore Boundless Ideas on an Open Source Blogging Platform
+      </p>
+      <div className="flex justify-center gap-3">
+        <Link href="/dashboard" className="btn btn-primary">
+          Get Started
+        </Link>
+        <Link
+          href="https://github.com/gibsol/blogue"
+          className="btn btn-primary"
+        >
+          Run Locally
+        </Link>
+      </div>
+    </Hero>
+  );
 }

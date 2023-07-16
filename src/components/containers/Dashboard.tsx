08ff1e07141ec2ec0eba50a6d4f472deb2c94b49
Gibsol <gibsol75@gmail.com>
2023-07-16 03:59:02 +0300

    :lipstick: Dashboard page Layout implementation

---
+++ src/components/containers/Dashboard.tsx
@@ -0,0 +1,12 @@
+import { ReactNode } from "react";
+
+import { Header } from "./Header";
+
+export const Dashboard = ({ children }: { children: ReactNode }) => {
+  return (
+    <div className="grid justify-center gap-5">
+      <Header body="Dashboard" />
+      <div className="grid gap-5 md:grid-cols-2 lg:grid-cols-3">{children}</div>
+    </div>
+  );
+};

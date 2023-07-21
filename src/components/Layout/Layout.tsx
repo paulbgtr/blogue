c6d711e70d41432776164e6ecaf0bf620e281e99
Gibsol <gibsol75@gmail.com>
2023-07-21 16:55:46 +0300

    :technologist: Layout isNavbarNeeded function impl
    This function will return a boolean that shows, whether to render Navbar
    on a page or not.

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -7,9 +7,20 @@ import { Footer } from "./Footer";
 export const Layout = ({ children }: { children: ReactNode }) => {
   const router = useRouter();
 
+  const isNavbarNeeded = () => {
+    return (
+      router.pathname !== "/login" &&
+      router.pathname !== "/login" &&
+      router.pathname !== "/sign_up" &&
+      router.pathname !== "/404" &&
+      router.pathname !== "/" &&
+      router.pathname !== "/password_reset"
+    );
+  };
+
   return (
     <>
-      {router.pathname !== "/" && <Navbar />}
+      {isNavbarNeeded() && <Navbar />}
       <div className="min-h-screen">
         <div className="p-6 lg:p-16 md:p-12">{children}</div>
       </div>
